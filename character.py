a62e5e27b955b72b3ce2c0fe1cfd605dedb9ee9c
AnodeGrindYo <mr.a.godoy@gmail.com>
2024-06-25 13:02:11 +0200

    Ajout de la méthode get_damage à la classe Character et tests unitaires associés

--- character.py
+++ character.py
@@ -3,3 +3,11 @@ class Character:
         self.name = name
         self.hp = 100
         self.is_alive = True
+
+    def get_damage(self, damage):
+        if damage < 0:
+            raise ValueError("damage should be positive")
+        self.hp -= damage
+        if self.hp <= 0:
+            self.hp = 0
+            self.is_alive = False
--- test/test_character.py
+++ test/test_character.py
@@ -11,3 +11,21 @@ def test_character_hp_when_created():
 def test_character_is_alive_when_created():
     c = Character("Albert")
     assert c.is_alive == True
+
+def test_character_get_damage():
+    c = Character("Albert")
+    c.get_damage(10)
+    assert c.hp == 90
+
+def test_character_get_damage_negative():
+    c = Character("Albert")
+    try:
+        c.get_damage(-10)
+    except ValueError as e:
+        assert str(e) == "damage should be positive"
+
+def test_character_is_dead_when_hp_is_zero():
+    c = Character("Albert")
+    c.get_damage(100)
+    assert c.hp == 0
+    assert c.is_alive == False
