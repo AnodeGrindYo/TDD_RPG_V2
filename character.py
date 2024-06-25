4b6f15a61a1ad341e32ef62ec26aaa6a277923d1
AnodeGrindYo <mr.a.godoy@gmail.com>
2024-06-25 12:52:02 +0200

    Ajout de l'attribut hp à la classe Character et test unitaire associé

--- character.py
+++ character.py
@@ -1,3 +1,4 @@
 class Character:
     def __init__(self, name):
         self.name = name
+        self.hp = 100
--- test/test_character.py
+++ test/test_character.py
@@ -3,3 +3,7 @@ from character import Character
 def test_character_name():
     c = Character("Albert")
     assert c.name == "Albert"
+
+def test_character_hp():
+    c = Character("Albert")
+    assert c.hp == 100
