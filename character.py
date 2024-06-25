2f457a64cb1af6f25e42af5e4bf108bbfb897779
AnodeGrindYo <mr.a.godoy@gmail.com>
2024-06-25 12:56:23 +0200

    Ajout de l'attribut is_alive à la classe Character et test unitaire associé

--- character.py
+++ character.py
@@ -2,3 +2,4 @@ class Character:
     def __init__(self, name):
         self.name = name
         self.hp = 100
+        self.is_alive = True
--- test/test_character.py
+++ test/test_character.py
@@ -4,6 +4,10 @@ def test_character_name():
     c = Character("Albert")
     assert c.name == "Albert"
 
-def test_character_hp():
+def test_character_hp_when_created():
     c = Character("Albert")
     assert c.hp == 100
+
+def test_character_is_alive_when_created():
+    c = Character("Albert")
+    assert c.is_alive == True
