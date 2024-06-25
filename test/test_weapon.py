bc1e98c0b1354943866bceb0280cd8b8f4b3f6a3
AnodeGrindYo <mr.a.godoy@gmail.com>
2024-06-25 15:50:28 +0200

    Ajout de la méthode get_damage à la classe Weapon et tests unitaires associés

--- test/test_weapon.py
+++ test/test_weapon.py
@@ -4,3 +4,7 @@ def test_weapon_creation():
     sword = Weapon("Sword", 10)
     assert sword.name == "Sword"
     assert sword.damage == 10
+
+def test_weapon_get_damage():
+    sword = Weapon("Sword", 10)
+    assert sword.get_damage() == 10
